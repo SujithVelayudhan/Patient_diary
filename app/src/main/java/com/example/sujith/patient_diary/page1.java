0a6dc4fbcec36e3e4b50cc8c471fe7c057d48471
SujithVelayudhan <sujithvelayudhanhere@gmail.com>
2018-12-07 11:12:36 +0530

    before pat log java

--- app/src/main/java/com/example/sujith/patient_diary/page1.java
+++ app/src/main/java/com/example/sujith/patient_diary/page1.java
@@ -1,12 +1,17 @@
 package com.example.sujith.patient_diary;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class page1 extends AppCompatActivity {
+
+    LinearLayout patient_layoutp;
 
 
 
@@ -14,6 +19,18 @@ public class page1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page1);
+
+        patient_layoutp=(LinearLayout)findViewById(R.id.patient_layout);
+
+        patient_layoutp.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent i=new Intent(page1.this,patient_registration.class);
+                startActivity(i);
+            }
+        });
 
 
     }
--- app/src/main/java/com/example/sujith/patient_diary/patient_registration.java
+++ app/src/main/java/com/example/sujith/patient_diary/patient_registration.java
@@ -2,12 +2,27 @@ package com.example.sujith.patient_diary;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.RequestParams;
 
 public class patient_registration extends AppCompatActivity {
+
+    AsyncHttpClient client;
+    RequestParams params;
+
+    EditText patemailp,patpassp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_patient_registration);
+
+        client=new AsyncHttpClient();
+        params=new RequestParams();
+
+        patemailp=(EditText)findViewById(R.id.pat_email);
+        patpassp=(EditText)findViewById(R.id.pat_password);
     }
 }
